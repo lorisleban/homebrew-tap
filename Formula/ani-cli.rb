1800d138f064f7c410c411187392921991397c91
lorisleban <loris.leban06@gmail.com>
2026-04-30 09:53:58 +0200

    update ani-cli

--- Formula/ani-cli.rb
+++ Formula/ani-cli.rb
@@ -1,8 +1,8 @@
 class AniCli < Formula
   desc "Rust TUI anime client"
   homepage "https://github.com/lorisleban/ani-cli"
-  url "https://github.com/lorisleban/ani-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "f5345e9e3b782dad882382f5281c0dccf433e9e315781ac16e956ab4b8e8e0cd"
+  url "https://github.com/lorisleban/ani-cli/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "8698a089fdd29660d2a0ddc5986b7d02b0b087b5decf9d3979654c2950bb465c"
   license "GPL-3.0-only"
 
   depends_on "rust" => :build
